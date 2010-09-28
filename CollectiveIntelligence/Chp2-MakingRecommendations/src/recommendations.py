ab57a835d62577691758d5b6b8d8f04be106c24b
javier.barreiro <javier.barreiro@09aa76a3-6042-0410-af02-65c79cd6b76c>
2010-09-28 04:41:38 +0000

    added getRecommendations()

--- CollectiveIntelligence/Chp2-MakingRecommendations/src/recommendations.py
+++ CollectiveIntelligence/Chp2-MakingRecommendations/src/recommendations.py
@@ -49,6 +49,53 @@ def sim_pearson(prefs,person1,person2):
     r = num/den
     return r
 
+
+# Returns the best matches for person from the prefs dictionary.
+# number of results and similarity function are optional params.
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+    scores = [(similarity(prefs,person,other),other) 
+              for other in prefs if other != person]
+    
+    #Sort the list so that top scores appear at the top
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
+    
+    
+# Get recommendations for a person by using a weighted average of every other user's rankings
+def getRecommendations(prefs,person,similarity=sim_pearson): 
+    totals={}
+    simSums={}
+    
+    for other in prefs:
+        #don't compare to myself
+        if (other==person): continue
+        
+        sim = similarity(prefs,person,other)
+        
+        #ignore scores of 0 or lower
+        if (sim<=0): continue
+        
+        for item in prefs[other]:
+            #only score movies I haven't seen yet
+            if (item not in prefs[person] or prefs[person][item]==0):
+                # Similarity * Score
+                totals.setdefault(item,0)
+                totals[item]+=prefs[other][item]*sim
+                # Sum of similarities
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
+                
+    # Create the normalized list
+    rankings=[(total/simSums[item],item) for item,total in totals.items()]
+    # return  the sorted list
+        
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+    
+           
+    
 # A dictionary of movie critics and their ratings on a small set of movies
 critics={
          'Lisa Rose':        {'Lady in the Water':2.5, 'Snakes on a Plane':3.5, 'Just My Luck':3.0, 'Superman Returns':3.5, 'You, Me and Dupree':2.5, 'The Night Listener':3.0},
@@ -60,4 +107,4 @@ critics={
          'Toby':             {                         'Snakes on a Plane':4.5,                     'Superman Returns':4.0, 'You, Me and Dupree':1.0}
 }
 
-print sim_pearson(critics,'Lisa Rose','Gene Seymour')
+#print sim_pearson(critics,'Lisa Rose','Gene Seymour')
