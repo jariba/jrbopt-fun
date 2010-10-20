6bfa300e71651c4f891442821fd7636e6d742fe9
javier.barreiro <javier.barreiro@09aa76a3-6042-0410-af02-65c79cd6b76c>
2010-10-20 00:43:57 +0000

    added generateFeedVector()

--- CollectiveIntelligence/Chp3-DiscoveringGroups/src/generatefeedvector.py
+++ CollectiveIntelligence/Chp3-DiscoveringGroups/src/generatefeedvector.py
@@ -13,14 +13,14 @@ def getWordCounts(url):
         else: summary=e.description
         
         #Extract a list of words
-        words=getwords(e.title+' '+summary)
+        words=getWords(e.title+' '+summary)
         for word in words:
             wc.setdefault(word,0)
             wc[word]+=1
             
     return d.feed.title,wc
 
-def getwords(html):
+def getWords(html):
     # remove all the html tags
     txt = re.compile(r'<[^>]+>').sub('',html)
     
@@ -30,3 +30,36 @@ def getwords(html):
     # convert to lower case
     return [word.lower() for word in words if word!='']
 
+def generateFeedVector(inputFile,outputFile):
+    apcount={}
+    wordcounts={}
+    feedlist=[]
+    
+    for feedurl in file(inputFile):
+        title,wc=getWordCounts(feedurl)
+        feedlist.append(title)
+        wordcounts[title]=wc
+        for word,count in wc.items:
+            apcount.setdefault(word,0)
+            if count>1:
+                apcount[word]+=1
+        
+        # Only include words that are not too common or too rare        
+        wordlist=[]
+        for word,blogCount in apcount.items():
+            frac=float(blogCount)/len(feedlist)
+            if (frac>0.1 and frac<0.5): wordlist.append(word)
+            
+        out=file(outputFile,'w')
+        out.write('Blog')
+        for word in wordlist: out.write('\t%s' % word)
+        out.write('\n')
+        for blog,wc in wordcounts.items():
+            out.write(blog)
+            for word in wordlist:
+                if word in wc: out.write('\t%d' % wc[word])
+                else: out.write('\t0')
+            out.write('\n')
+            
+            
+generateFeedVector('data/feedlist.txt','blogdata.txt')            
